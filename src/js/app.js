0456df945a2cfebd421c8270e077eed93bc849af
hevivancos <vivancos.ramon@gmail.com>
2022-02-19 09:29:58 +0000

    ejercicio hecho

--- src/js/app.js
+++ src/js/app.js
@@ -26,21 +26,21 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`; //$ te deja contactenar string y variables
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>Isaac Vivancos</h1>
           <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h3>Alicante</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://mentaychocolate.es/"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/hevivancos?tab=repositories"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.instagram.com/hevivancos/"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +54,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://mentaychocolate.es/wp-content/uploads/2016/05/cascada.png",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://mentaychocolate.es/wp-content/uploads/2016/05/isaac-250x250.png",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
